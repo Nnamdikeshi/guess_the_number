792f0fcd611b0a86cf345341c0f6eb258c243c6a
njk <Nnamdi@midwest.com>
2018-01-16 14:41:13 -0600

    added validation

--- guess_the_number.py
+++ guess_the_number.py
@@ -17,9 +17,16 @@ def generate_secret(low, high):
 
 def get_guess():
     '''get user's guess'''
-    return int(input('Guess the secret number? '))
+    while True:
+        try:
+            guess = int(input('Guess the secret number? '))       
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return guess 
 
-
+			
 def check_guess(guess, secret):
     '''compare guess and secret, return string describing result of comparison'''
     if guess == secret:
